3286d94229fd4616cd3544123c543d4f9641ca43
Rachitmehrotra <Rachit.Mehrotra@in.imshealth.com>
2019-12-30 10:54:43 +0530

    made some change in security group

--- servers.tf
+++ servers.tf
@@ -15,6 +15,7 @@ resource "aws_instance" "jenkins" {
 	      sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
               sudo echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
               sudo apt-get update -y
+              sudo apt-get install openjdk-8-jdk
               sudo apt-get install ansible -y
               sudo apt-get install jenkins -y
               sudo systemctl start jenkins
